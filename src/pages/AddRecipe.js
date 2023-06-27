e0e031142051a7fe6ecac058a4b93cd3e0a4368b
Ahmed <ahmed.khalid.abdalftah@gmail.com>
2023-06-27 15:16:13 -0700

    modify delete button icon & create reordering arrows

--- src/pages/AddRecipe.js
+++ src/pages/AddRecipe.js
@@ -5,19 +5,38 @@ import { useForm } from "react-hook-form";
 import InputAdornment from '@mui/material/InputAdornment';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import { MdDelete } from "react-icons/md";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { FaArrowsAltV } from "react-icons/fa"
+import { AiFillCloseCircle } from "react-icons/ai"
 
-const schema = yup.object({
-    title: yup.string().required('This Field Required.'),
-    ingre: yup.string().required('This Field Required.'),
-    dis: yup.string().required('This Field Required.')
-}).required();
+// const schema = yup.object({
+//     title: yup.string().required('This Field Required.'),
+//     ingre: ingredients.length > 0 ? "" : yup.string().required('This Field Required.'),
+//     des: yup.string().required('This Field Required.')
+// }).required();
 const AddRecipe = () => {
-    const { register, handleSubmit, watch, formState: { errors }, reset } = useForm({
+    const [ingredients, setIngredients] = useState([]);
+
+    const schema = yup.object({
+        title: yup.string().required('This Field Required.'),
+        ingre: ingredients.length > 0 ? "" : yup.string().required('This Field Required.'),
+        des: yup.string().required('This Field Required.')
+    }).required();
+
+    const { register, handleSubmit, watch, formState: { errors }, reset, resetField } = useForm({
         resolver: yupResolver(schema)
     });
-    const [ingredients, setIngredients] = useState([]);
+
+    const [backEndData, setBackEndData] = useState({});
+
     const onSubmit = data => {
-        data['ingre'] = ingredients;
+
+        setBackEndData({
+            title: data.title,
+            ingredients: ingredients,
+            description: data.des
+        })
         console.log(data)
         reset();
         setIngredients([]);
@@ -28,12 +47,33 @@ const AddRecipe = () => {
         if (ingerd.trim().length > 0) {
             setIngredients(old => [...old, ingerd])
         }
+
+    }
+    const onDragEnd = (result) => {
+        if (!result.destination) {
+            return;
+        }
+
+        const reorderedItems = Array.from(ingredients);
+        const [removed] = reorderedItems.splice(result.source.index, 1);
+        reorderedItems.splice(result.destination.index, 0, removed);
+        console.log('reorderedItemsk', reorderedItems)
+
+        setIngredients(reorderedItems);
+        console.log('result', result)
+        console.log("reorderedItems", reorderedItems)
+    };
+
+    const deleteInputIngredients = () => {
+        resetField('ingre')
     }
 
-    const deleteIngredientsHandler = (ing) => {
-        const newIngredientsArr = ingredients.filter(ingre => ingre !== ing);
+    const deleteIngredientsHandler = (ind) => {
+        const newIngredientsArr = ingredients.filter((_, index) => index !== ind);
         setIngredients(newIngredientsArr)
     }
+    console.log("backEndData", backEndData)
+    console.log("ingredients", ingredients)
 
     return <Stack paddingTop={'30px'} alignItems={'center'}>
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -45,10 +85,8 @@ const AddRecipe = () => {
                         <Typography color={'#c94343'}>{errors.title?.message}</Typography>
                     </Stack>
                 </Stack>
-                <Stack spacing={1} justifyContent={'space-between'}>
-                    <Stack direction={'row'} justifyContent={'space-between'}>
-                        <Typography fontSize={'20px'} fontWeight={'bold'} color={'#873e6c'}>Ingredients:</Typography>
-                    </Stack >
+                <Stack direction={'row'} spacing={1} justifyContent={'space-between'} alignItems={'center'}>
+                    <Typography fontSize={'20px'} fontWeight={'bold'} color={'#873e6c'}>Ingredients:</Typography>
                     <Stack>
                         <Stack alignItems={'flex-end'}>
                             <Card sx={{ width: '600px', minHeight: "150px" }}>
@@ -61,29 +99,43 @@ const AddRecipe = () => {
                                             InputProps={{
                                                 endAdornment: (
                                                     <InputAdornment position="end">
-                                                        <Button onClick={addIng} variant="contained">
-                                                            Add
-                                                        </Button>
+                                                        <Stack direction={'row'} spacing={1} alignItems={'center'}>
+                                                            <AiFillCloseCircle cursor={'pointer'} onClick={deleteInputIngredients} />
+                                                            <Button onClick={addIng} variant="contained">
+                                                                Add
+                                                            </Button>
+                                                        </Stack>
                                                     </InputAdornment>
                                                 ),
                                             }}
                                         />
                                     </Stack>
-                                    <ul>
-                                        {ingredients.map((ing, index) => {
-                                            return <li key={index}>
-                                                <Stack direction={'row'} spacing={1} justifyContent={'space-between'} alignItems={'center'}>
-                                                    <Typography >{ing}</Typography>
-                                                    <Button onClick={() => deleteIngredientsHandler(ing)}>delete</Button>
-                                                </Stack>
-                                            </li>
-                                        })}
-                                    </ul>
+                                    <DragDropContext onDragEnd={onDragEnd}>
+                                        <Droppable droppableId={`${ingredients}`}>
+                                            {(provided) => (
+                                                <ul style={{ listStyleType: 'none' }}  {...provided.droppableProps} ref={provided.innerRef}>
+                                                    {ingredients.map((ing, index) => {
+                                                        return <Draggable key={ing} draggableId={ing} index={index}>
+                                                            {(provided) => (
+                                                                <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >
+                                                                    <Stack direction={'row'} spacing={2} justifyContent={'space-between'} alignItems={'center'}>
+                                                                        <Typography ><FaArrowsAltV /> {ing}</Typography>
+                                                                        <MdDelete size={20} cursor={'pointer'} onClick={() => deleteIngredientsHandler(index)} />
+                                                                    </Stack>
+                                                                </li>
+                                                            )}
+                                                        </Draggable>
+                                                    })}
+                                                    {provided.placeholder}
+                                                </ul>
+                                            )}
+                                        </Droppable>
+                                    </DragDropContext>
                                 </CardContent>
                             </Card>
 
                         </Stack>
-                        <Stack paddingLeft={'200px'} >
+                        <Stack  >
                             <Typography color={'#c94343'}>{errors.ingre?.message}</Typography>
                         </Stack>
                     </Stack>
@@ -92,13 +144,13 @@ const AddRecipe = () => {
                     <Typography fontSize={'20px'} fontWeight={'bold'} color={'#873e6c'}>Description:</Typography>
                     <Stack>
                         <TextField
-                            {...register('dis')}
+                            {...register('des')}
                             multiline
                             rows={4}
                             variant="outlined"
                             sx={{ width: "600px" }}
                         />
-                        <Typography color={'#c94343'}>{errors.dis?.message}</Typography>
+                        <Typography color={'#c94343'}>{errors.des?.message}</Typography>
                     </Stack>
                 </Stack>
                 <Stack direction={'row'} justifyContent={'flex-end'}>
